a34467cc7dbdcd5678f6df0d5a400d3fdc5f33cb
Valnut0206 <105207959+Valnut0206@users.noreply.github.com>
2022-06-22 17:34:48 +0000

    Js. Calculator

---
+++ js.life.js
@@ -0,0 +1,13 @@
+var opr = prompt("Operator:");
+var Number1 = prompt("Number1: ");
+var Number2 = prompt("Number2: ");
+
+if (opr == "+") {
+  prompt(Number1 + Number2);
+} else if (opr == "-") {
+  prompt(Number1 - Number2);
+} else if (opr == "*") {
+  prompt(Number1 * Number2);
+} else if (opr == "/") {
+  prompt(Number1 / Number2);
+}
